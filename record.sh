6f8327e04ad718ef350548b6b131083fa520077b
Arina Okshus <45241701+chameleon-lizard@users.noreply.github.com>
2019-05-07 11:15:24 +0300

    Merged HD and FHD record scripts
    Now the script is checking the screen resolution and uses it to record a video.

---
+++ record.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+RESOLUTION=$(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/')
+NAME=$(date +"%F-%H-%M-%S-$RESOLUTION.mp4")
+
+echo "Recording into ~/Videos/Recordings/$NAME..."
+
+ffmpeg -loglevel panic -video_size $RESOLUTION -framerate 25 -f x11grab -i :0.0+0,0 $NAME
+
+mv $NAME /home/chameleon/Videos/Recordings
