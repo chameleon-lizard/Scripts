a7cd4fc1dc8308337b59a3ef99196097dc99eff3
Arina Okshus <45241701+chameleon-lizard@users.noreply.github.com>
2019-11-18 10:12:59 +0000

    Changed i3lock

--- my-i3lock-config.sh
+++ my-i3lock-config.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-PICTURE=/tmp/i3lock.png
-SCREENSHOT="scrot $PICTURE"
+insidecolor=00000000
+ringcolor=ffffffff
+keyhlcolor=d23c3dff
+bshlcolor=d23c3dff
+separatorcolor=00000000
+insidevercolor=00000000
+insidewrongcolor=d23c3dff
+ringvercolor=ffffffff
+ringwrongcolor=ffffffff
+verifcolor=ffffffff
+timecolor=ffffffff
+datecolor=ffffffff
+loginbox=00000066
+font="sans-serif"
+locktext='Type password to unlock...'
+lock_timeout=5
 
 BLUR="5x4"
-
-$SCREENSHOT
+PICTURE=$(cat ~/.fehbg | awk '{ print $4 }' | tr -d "'")
 convert $PICTURE -blur $BLUR $PICTURE
-i3lock -i $PICTURE
-rm $PICTURE
+i3lock \
+        -t -i $PICTURE \
+        --timepos='x+110:h-70' \
+        --datepos='x+43:h-45' \
+        --clock --date-align 1 --datestr "$locktext" \
+        --insidecolor=$insidecolor --ringcolor=$ringcolor --line-uses-inside \
+        --keyhlcolor=$keyhlcolor --bshlcolor=$bshlcolor --separatorcolor=$separatorcolor \
+        --insidevercolor=$insidevercolor --insidewrongcolor=$insidewrongcolor \
+        --ringvercolor=$ringvercolor --ringwrongcolor=$ringwrongcolor --indpos='x+280:h-70' \
+        --radius=20 --ring-width=4 --veriftext='' --wrongtext='' \
+        --verifcolor="$verifcolor" --timecolor="$timecolor" --datecolor="$datecolor" \
+        --time-font="$font" --date-font="$font" --layout-font="$font" --verif-font="$font" --wrong-font="$font" \
+        --noinputtext='' --force-clock --pass-media-keys $lockargs 
