033b412457e753f39f84aab9f065701dfe6d85a3
Arina Okshus <45241701+chameleon-lizard@users.noreply.github.com>
2019-11-20 11:43:49 +0000

    Fixed blurred wallpapers when used with i3lock

--- random_wallpapers.sh
+++ random_wallpapers.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 IMAGE=$(wpg -m | grep .config)
-
 feh --bg-fill ${IMAGE}
+
+rm -rf ~/.config/wpg/wallpapers
+mkdir ~/.config/wpg/wallpapers
+cp ~/Pictures/Wallpapers/* ~/.config/wpg/wallpapers
 
 ~/.telegram-palette-gen/telegram-palette-gen --wal
